1eb088371f6e944b9c78cefc84008f497c82cb8f
tu le <tu@opsys.cs.umsl.edu>
2025-02-09 22:27:03 -0600

    second commit for updated version

--- user.c
+++ user.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char*argv) {
+int main(int argc, char **argv) {
 	if (argc!=2) {
-	fprintf(stderr, "Usage: %s <number_of_iterations>\n", argv);
+	fprintf(stderr, "Usage: %s <number_of_iterations>\n", argv[0]); 
 	exit(1);
 	}
 
-	int iterations = atoi(argv);
+	int iterations = atoi(argv[1]);
 
 	for(int i = 1; i <= iterations; i++) {
 		printf("Greetings from child process %d! (Parent is %d)\n", getpid(), getppid());
@@ -16,6 +16,6 @@ int main(int argc, char*argv) {
 		sleep(1);
 		printf("Iteration %d: Ah, I'm waking up again!\n",  i);
 	}
-	printf("Child process %d signing off. Peace!\n, getpid());
+	printf("Child process %d signing off. Peace!\n", getpid());
 	return 0;
 }
